66bf854d0763e6beb31d6b08a80d0868b40d3228
Crunchypack <simon.wolf.lobo@gmail.com>
2021-05-10 21:37:14 -0400

    Finished Observable HeroService

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Hero } from '../hero'; 
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-hero-detail',
@@ -7,11 +8,18 @@ import { Hero } from '../hero';
   styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
+  heroes: Hero[] = [];
 
   @Input() hero?: Hero; // Make hero property available from binding
-  constructor() { }
+  constructor(private heroService : HeroService) { }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
 
   ngOnInit(): void {
+    this.getHeroes();
   }
 
 }
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+
+@Injectable({ // Class participates in dependency injection system
+  providedIn: 'root' // Metadata object
+  // creates single shared instance of the service, and injects to any class that asks for it.
 })
 export class HeroService {
+
+  getHeroes(): Observable<Hero[]>{
+    const heroes = of(HEROES);
+  return heroes;
+  }
 
   constructor() { }
 }
